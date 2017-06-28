339a5da1af853b4f374651d2d7be54bb00534317
Syrimande <caterinadasilva@gmail.com>
2017-06-28 14:55:39 -0400

    fixes diseño

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 			if (name == "" && message == "") {
 				alert("Debes escribir tu nombre y mensaje");
 			} else {
-				$("#comentarios").append('<div><span class="center-align"' + name + '</span><p class="center-align">' + message + '</p></div>');
+				$("#comentarios").append('<div class="card red-text text-lighten-1 comentario col s8 offset-s2 white"><div class="card-content"><span class="card-title center-align">' + name + '</span><p class="center-align">' + message + '</p></div></div>');
 			}
 		});
 });
